a2d3d7031271fca9a6c5fc5d14ffc2820ab4ac92
sailorhdx <sailorhdx@hotmail.com>
2017-06-19 16:10:37 +0800

    Update： click(6.6>>6.7) pycrypto(2.6.1) IPy(0.83) idna(2.1>>2.5) ipaddress(1.0.16>>1.0.18) asn1crypto(NULL>>0.22.0) enum34(1.1.6) cryptography(1.4>>1.9)

--- lib/python2.7/site-packages/cryptography/hazmat/backends/openssl/utils.py
+++ lib/python2.7/site-packages/cryptography/hazmat/backends/openssl/utils.py
@@ -4,23 +4,22 @@
 
 from __future__ import absolute_import, division, print_function
 
-import six
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric.utils import Prehashed
 
 
-def _truncate_digest(digest, order_bits):
-    digest_len = len(digest)
+def _calculate_digest_and_algorithm(backend, data, algorithm):
+    if not isinstance(algorithm, Prehashed):
+        hash_ctx = hashes.Hash(algorithm, backend)
+        hash_ctx.update(data)
+        data = hash_ctx.finalize()
+    else:
+        algorithm = algorithm._algorithm
 
-    if 8 * digest_len > order_bits:
-        digest_len = (order_bits + 7) // 8
-        digest = digest[:digest_len]
+    if len(data) != algorithm.digest_size:
+        raise ValueError(
+            "The provided data must be the same length as the hash "
+            "algorithm's digest size."
+        )
 
-    if 8 * digest_len > order_bits:
-        rshift = 8 - (order_bits & 0x7)
-        assert 0 < rshift < 8
-
-        mask = 0xFF >> rshift << rshift
-
-        # Set the bottom rshift bits to 0
-        digest = digest[:-1] + six.int2byte(six.indexbytes(digest, -1) & mask)
-
-    return digest
+    return (data, algorithm)
